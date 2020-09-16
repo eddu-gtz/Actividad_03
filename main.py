a20632a40421cfbd99b0b92f0b3eeb9d65919376
Eduardo Gutierrez <eduardo.gutierrez7981@alumnos.udg.mx>
2020-09-16 00:20:37 -0500

    Imprime menu, guarda el nombre y lo imprime

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hola mundo!")
+while True:
+    print("1. Agregar")
+    print("2. Mostrar")
+    print("0. Salir")
+    op = input()
+
+    if op == "1":
+        nombre = input("Nombre: ")
+    elif op == "2":
+        print(nombre)
+    elif op == "0":
+        break
